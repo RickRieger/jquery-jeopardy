ce24c405e559b3ce8946e41bae6890c27881a7b4
Rick Rieger <rickrieger36251@gmail.com>
2023-04-07 18:33:41 -0400

    corrected spelling in readme, and added a toggle featuire with a input checkbox button and a key value store in local storage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jQuery Jeopardy
 
-This was a fun project!  I had a "ah-ha" moment laying in bed one night when I thought of how I would add animation to mimic the show as it happens on television.  A quick google search led me to available sounds to incorperate and  behold, a complete Jeopardy game.  The answers are logged in the console and you can check them out if you get stuck using the chrome dev tools. Occasionally, the game generates empty questions and I have not taken the time to search the very large file containing the questions.  In this case, just hit "new game".  
+This was a fun project! I had a "ah-ha" moment laying in bed one night when I thought of how I would add animation to mimic the show as it happens on television. A quick google search led me to available sounds to incorporate and behold, a complete Jeopardy game. The answers are logged in the console and you can check them out if you get stuck using the chrome dev tools. Occasionally, the game generates empty questions and I have not taken the time to search the very large file containing the questions. In this case, just hit "new game".
 
 Below are the instructions I followed.
 
@@ -12,13 +12,13 @@ Check out a demo of what this might look like below:
 
 ## Requirements
 
-* A grid of Jeopardy dollar amounts are displayed on the screen
-* Upon clicking an dollar amount, a question is displayed
-* The user can submit an answer
-  * If they are correct, their score is updated
-* The user's score should be saved even if the page is refreshed
-* jQuery is used throughout the Javascript instead of `document.createElement`, `document.querySelector`, etc.
-* Your project is hosted on GitHub pages
+- A grid of Jeopardy dollar amounts are displayed on the screen
+- Upon clicking an dollar amount, a question is displayed
+- The user can submit an answer
+  - If they are correct, their score is updated
+- The user's score should be saved even if the page is refreshed
+- jQuery is used throughout the Javascript instead of `document.createElement`, `document.querySelector`, etc.
+- Your project is hosted on GitHub pages
 
 ## Overview
 
@@ -38,7 +38,7 @@ const QUESTIONS = [
   "..."
 ```
 
-You'll use this data file to get Jeopardy questions and answers. 
+You'll use this data file to get Jeopardy questions and answers.
 
 For the base assignment, if the user clicks a $200 question, the app will display a random $200 question from the dataset (and same with the other dollar amounts).
 
@@ -46,12 +46,11 @@ For the base assignment, if the user clicks a $200 question, the app will displa
 
 #### 1. Create your files
 
-* Start off by creating an `index.html`, `main.js`, and `styles.css`
-* In `index.html`, add two `script` tags to import first `jeopardy.js` followed by `main.js`
-* Also add a `link` tag to import `styles.css`
-* Finally, add `jquery` to your `index.html`. You can do this by adding the following line to your `<head>`
-  * `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>`
-
+- Start off by creating an `index.html`, `main.js`, and `styles.css`
+- In `index.html`, add two `script` tags to import first `jeopardy.js` followed by `main.js`
+- Also add a `link` tag to import `styles.css`
+- Finally, add `jquery` to your `index.html`. You can do this by adding the following line to your `<head>`
+  - `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>`
 
 #### 2. Build the layout of dollar amounts
 
@@ -61,7 +60,7 @@ Also, add some style rules in `styles.css` to make things look reasonable.
 
 #### 3. Get a Jeopardy question
 
-Now, make it so that when one of your $100, $200, etc. buttons are pressed, a random question of that dollar value is displayed. 
+Now, make it so that when one of your $100, $200, etc. buttons are pressed, a random question of that dollar value is displayed.
 
 To do this, use the global `QUESTIONS` variable defined in `jeopardy.js`. When one of the dollar amounts is clicked, you can filter the questions to only those with the correct dollar amount. Then, choose one of those questions randomly. It's okay that a different question is chosen each time even when you press the same button.
 
@@ -91,9 +90,8 @@ Finally, add code so that when the page loads, the browser checks local storage 
 
 ## Stretch goals
 
-* Make a square become grey and un-clickable after being chosen.
+- Make a square become grey and un-clickable after being chosen.
 
-* Instead getting a random question every time a button is pressed, populate the game with the questions from a single past Jeopardy game. To do this, randomly choose a valid 'show number' (see `jeopardy.js` data) and use all the questions with that show number. As part of this, display a category (found in the data) over each column of $ amounts.
+- Instead getting a random question every time a button is pressed, populate the game with the questions from a single past Jeopardy game. To do this, randomly choose a valid 'show number' (see `jeopardy.js` data) and use all the questions with that show number. As part of this, display a category (found in the data) over each column of $ amounts.
 
-* Expanding on the previous two stretch goals, make the game fully refresh-able. Grayed out squares should stay grayed out on refresh and the same questions and categories that were chosen for the game should persist on refresh as well. You'll probably want to add a reset button as part of these changes.
-
+- Expanding on the previous two stretch goals, make the game fully refresh-able. Grayed out squares should stay grayed out on refresh and the same questions and categories that were chosen for the game should persist on refresh as well. You'll probably want to add a reset button as part of these changes.
--- main.js
+++ main.js
@@ -1,125 +1,126 @@
-// Grey's out DIV and restricts a click listener
-function removeSquareFromJeopardyBoard(elementID){
-    $(`#${elementID}`).removeClass('clickable');
-    $(`#${elementID}`).css("background-color", "grey");
-    $(`#${elementID}`).off("click",mainFunctionWhenSquareIsClicked);
-    greyedOutSquaresIDsArray.push(elementID);
-    window.localStorage.setItem('greyed-out-squares', greyedOutSquaresIDsArray);
-    console.log("The array below is a collection of 'greyed out' squares;")
-    console.log(greyedOutSquaresIDsArray);
+let button_toggle_value = $('#toggle_sound')[0].checked;
+
+console.log(button_toggle_value);
+
+$('#toggle_sound').change(function () {
+  button_toggle_value = $('#toggle_sound')[0].checked;
+  console.log('sucka! ======>', button_toggle_value);
+  //   const elems = document.querySelectorAll('audio');
+  //   for (const el of elems) {
+  //     el.muted = button_toggle_value;
+  //     el.pause();
+  //   }
+});
+
+let soundBoolean = window.localStorage.getItem('soundBoolean');
+if (!soundBoolean) {
+  window.localStorage.setItem('soundBoolean', false);
 }
 
-function removeSquareFromJeopardyBoard2(elementID){
-    console.log(`element ID in remove square from: ${elementID}`)
-    $(`#${elementID}`).removeClass('clickable');
-    $(`#${elementID}`).css("background-color", "grey");
-    $(`#${elementID}`).off("click",mainFunctionWhenSquareIsClicked);
+// Grey's out DIV and restricts a click listener
+function removeSquareFromJeopardyBoard(elementID) {
+  $(`#${elementID}`).removeClass('clickable');
+  $(`#${elementID}`).css('background-color', 'grey');
+  $(`#${elementID}`).off('click', mainFunctionWhenSquareIsClicked);
+  greyedOutSquaresIDsArray.push(elementID);
+  window.localStorage.setItem('greyed-out-squares', greyedOutSquaresIDsArray);
+  console.log("The array below is a collection of 'greyed out' squares;");
+  console.log(greyedOutSquaresIDsArray);
+}
+
+function removeSquareFromJeopardyBoard2(elementID) {
+  console.log(`element ID in remove square from: ${elementID}`);
+  $(`#${elementID}`).removeClass('clickable');
+  $(`#${elementID}`).css('background-color', 'grey');
+  $(`#${elementID}`).off('click', mainFunctionWhenSquareIsClicked);
 }
 greyedOutSquaresIDsArray = window.localStorage.getItem('greyed-out-squares');
 
+if (greyedOutSquaresIDsArray === null) {
+  greyedOutSquaresIDsArray = [];
+} else {
+  greyedOutSquaresIDsArray = greyedOutSquaresIDsArray.split(',');
 
-if (greyedOutSquaresIDsArray === null){
-
-    greyedOutSquaresIDsArray = [];
-
-}
-else{
-    greyedOutSquaresIDsArray = greyedOutSquaresIDsArray.split(',');
-
-    for(const square of greyedOutSquaresIDsArray){
-        removeSquareFromJeopardyBoard2(square);
-    }
-
+  for (const square of greyedOutSquaresIDsArray) {
+    removeSquareFromJeopardyBoard2(square);
+  }
 }
 
+function mainFunctionWhenSquareIsClicked() {
+  let elementId = $(this).attr('id');
+  console.log(`Element id that was clicked: ${elementId}`);
 
-function mainFunctionWhenSquareIsClicked(){
-    
-    let elementId = $(this).attr("id");
-    console.log(`Element id that was clicked: ${elementId}`);
-    
-    // makes the square greyed out and not 'clickable' by removing the class
-    removeSquareFromJeopardyBoard(elementId);
-    
-    // makes stuff disappear and other stuff appear
-    timedFunctionMakeJeopardyBoardDisappear();
-    timedFunctionCreateDivToHoldQuestion();
-    timedFunctionExpandDiv();
-    
-    // gradually decreases the opacity of jeopardy
-    $('.app').addClass('opacity');
-    
-    // whatever user clicks on sets value
-    dollars = $(this).text();
+  // makes the square greyed out and not 'clickable' by removing the class
+  removeSquareFromJeopardyBoard(elementId);
 
-    // transforms element's id to match corresponding category
+  // makes stuff disappear and other stuff appear
+  timedFunctionMakeJeopardyBoardDisappear();
+  timedFunctionCreateDivToHoldQuestion();
+  timedFunctionExpandDiv();
 
-    elementId = elementId.split('');
-    elementId = elementId.slice(0, -3);
-    elementId = elementId.join('');
-    console.log(`category is (from element id): ${elementId}`)
-    console.log(`dollar value of box clicked: ${dollars}`);
-    
-    // sorts all objects accordingly by dollar value into a new array
-    for (const question of ISOLATED_QUESTIONS){
-        let regexCategory = question.category;
-        regexCategory = regexCategory.replace(/[\W_]+/g,"");
-        if (question.value === dollars && regexCategory === elementId){
-            newQuestionsArray.push(question);
-        };
-    };
+  // gradually decreases the opacity of jeopardy
+  $('.app').addClass('opacity');
 
-    // console.log() to see the new array
-    console.log("below is the new array of questions based on user interaction:")
-    console.log(newQuestionsArray)
+  // whatever user clicks on sets value
+  dollars = $(this).text();
 
-    // isolate a random question and answer from the new array
-    questionObject = random(newQuestionsArray);
-    questionFocused = questionObject.question;
-    correctAnswer = questionObject.answer;
-    
-    // console.log() the question and answer for convenience 
-    console.log(questionObject);
-    console.log(`The question is: ${questionFocused}`);
-    console.log(`The answer is: ${correctAnswer}`);
+  // transforms element's id to match corresponding category
 
-    //sounds played when question is selected
-    if(questionObject.round === 'Double Jeopardy!'){
-       0
- 
+  elementId = elementId.split('');
+  elementId = elementId.slice(0, -3);
+  elementId = elementId.join('');
+  console.log(`category is (from element id): ${elementId}`);
+  console.log(`dollar value of box clicked: ${dollars}`);
+
+  // sorts all objects accordingly by dollar value into a new array
+  for (const question of ISOLATED_QUESTIONS) {
+    let regexCategory = question.category;
+    regexCategory = regexCategory.replace(/[\W_]+/g, '');
+    if (question.value === dollars && regexCategory === elementId) {
+      newQuestionsArray.push(question);
     }
-    else{
-        $('#populate-question-sound')[0].play();
-    }
+  }
 
-    // make new DOM element to display question
-    const newItem = $(`<h3 class = "question">${questionFocused}</h3>`);
-    
-    // display new DOM element
-    timedFunctionPlaceQuestion(newItem);
-    AddEventListenerAndGrabUserInput();
-    
-    // clears the array for the next question
-    newQuestionsArray = []; 
-};
+  // console.log() to see the new array
+  console.log('below is the new array of questions based on user interaction:');
+  console.log(newQuestionsArray);
 
+  // isolate a random question and answer from the new array
+  questionObject = random(newQuestionsArray);
+  questionFocused = questionObject.question;
+  correctAnswer = questionObject.answer;
 
+  // console.log() the question and answer for convenience
+  console.log(questionObject);
+  console.log(`The question is: ${questionFocused}`);
+  console.log(`The answer is: ${correctAnswer}`);
 
+  //sounds played when question is selected
+  if (questionObject.round === 'Double Jeopardy!') {
+    0;
+  } else {
+    $('#populate-question-sound')[0].play();
+  }
+
+  // make new DOM element to display question
+  const newItem = $(`<h3 class = "question">${questionFocused}</h3>`);
+
+  // display new DOM element
+  timedFunctionPlaceQuestion(newItem);
+  AddEventListenerAndGrabUserInput();
+
+  // clears the array for the next question
+  newQuestionsArray = [];
+}
 
 // This needs to be here for the intro/div color change
 jeopardySquaresArray = $('.jeopardy-children');
 
-
-
 // click listener for square
-$('.clickable').on("click" , mainFunctionWhenSquareIsClicked);
+$('.clickable').on('click', mainFunctionWhenSquareIsClicked);
 
 // click listener for new game button
-$('#clear-all').on("click" , ()=>{
-    window.localStorage.clear();
-    location.reload();
+$('#clear-all').on('click', () => {
+  window.localStorage.clear();
+  location.reload();
 });
-
-
-
-
