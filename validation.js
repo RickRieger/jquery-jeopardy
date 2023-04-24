2df0132fc6d8af3dc764fc0365481a265b9adb48
Rick Rieger <rickrieger36251@gmail.com>
2023-04-24 13:32:07 -0400

    changed url in validation.js

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 let showNum = window.localStorage.getItem("show-num");
 if (showNum) {
-  window.open("/jeopardy.html");
+  window.open("https://rickrieger.github.io/jquery-jeopardy/jeopardy.html");
 }
 
 $("form").on("keyup click", () => {
